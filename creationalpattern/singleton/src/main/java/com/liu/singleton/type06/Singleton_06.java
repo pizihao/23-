44c875d0676973ab96c73dd715f90f31183874cc
liuwenh <2335715300@qq.com>
2020-11-14 10:33:21 +0800

    双重检查的破坏

--- creationalpattern/singleton/src/main/java/com/liu/singleton/type06/Singleton_06.java
+++ creationalpattern/singleton/src/main/java/com/liu/singleton/type06/Singleton_06.java
@@ -1,14 +1,23 @@
 package com.liu.singleton.type06;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author shidacaizi
  * @date 2020/5/14 8:45
  */
 public class Singleton_06 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
 //        测试
         Singleton singleton = Singleton.getInstance();
-        Singleton singleton1 = Singleton.getInstance();
+
+        /*此处跳过了双重检查，直接通过反射调用了私有的构造函数，破坏了单例*/
+        Class<Singleton> singletonClass = Singleton.class;
+        Constructor<?> con = singletonClass.getDeclaredConstructor();
+        con.setAccessible(true);
+        Singleton singleton1 = (Singleton) con.newInstance();
         System.out.println(singleton == singleton1);
     }
 }
